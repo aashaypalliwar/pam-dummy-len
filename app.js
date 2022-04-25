a93463e465bdf9b98930d567ede4b737dca76b16
Aashay Palliwar <45427968+aashaypalliwar@users.noreply.github.com>
2022-04-25 11:56:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/:clientEndpoint', (req, res, next) => {
-    let len = (req.params.clientEndpoint).length;
+    let len = (req.params.clientEndpoint).length * 2;
     console.log("Length is : " + len)
     res.send({len});
 })
